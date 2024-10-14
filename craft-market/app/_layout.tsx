71ce2266467f703431facf03b2f0b7725e1e4a21
Enes koç <eneskoc172@gmail.com>
2024-10-14 20:16:52 +0300

    Update _layout.tsx

--- craft-market/app/_layout.tsx
+++ craft-market/app/_layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack initialRouteName="login">
        
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="login" 
