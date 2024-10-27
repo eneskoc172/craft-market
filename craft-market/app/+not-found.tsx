660028c06a750a540b6309f780749a3aac6ff5a2
Enes koç <eneskoc172@gmail.com>
2024-10-27 18:09:47 +0300

    Update +not-found.tsx

--- craft-market/app/+not-found.tsx
+++ craft-market/app/+not-found.tsx
@@ -1,18 +1,21 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
+import { Image, StyleSheet } from 'react-native';
+import React from 'react';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+
+      <Image source={require('../assets/images/notfound.png')} style={styles.mainImage} />
+        <ThemedText type="title">Page Not Found</ThemedText>
         <Link href="/" style={styles.link}>
           <ThemedText type="link">Go to home screen!</ThemedText>
         </Link>
+   
       </ThemedView>
     </>
   );
@@ -26,7 +29,18 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   link: {
-    marginTop: 15,
+    backgroundColor: '#FF6347',
     paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginBottom: 20,
+    
+  },
+  mainImage: {
+    width: '100%',
+    height: 250,
+    resizeMode: 'contain',
+    marginBottom: 20,
   },
 });
