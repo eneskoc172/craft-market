a090b7831a568e5b2ea775c5186c18ec3c61c987
Enes koç <eneskoc172@gmail.com>
2024-10-28 14:31:03 +0300

    Create change-password.tsx

---
+++ craft-market/app/change-password.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
+export default function ChangePasswordScreen({ navigation }) {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [verifyPassword, setVerifyPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      {/* Geri Dön Butonu ve Başlık */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.replace('explore2')}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>Change Password</Text>
+      </View>
+
+      {/* Eski Şifre Girişi */}
+      <TextInput
+        style={styles.input}
+        placeholder="Old Password"
+        secureTextEntry
+        value={oldPassword}
+        onChangeText={setOldPassword}
+      />
+
+      {/* Yeni Şifre Girişi */}
+      <TextInput
+        style={styles.input}
+        placeholder="New Password"
+        secureTextEntry
+        value={newPassword}
+        onChangeText={setNewPassword}
+      />
+
+      {/* Şifre Doğrulama Girişi */}
+      <TextInput
+        style={styles.input}
+        placeholder="Verify Password"
+        secureTextEntry
+        value={verifyPassword}
+        onChangeText={setVerifyPassword}
+      />
+
+      {/* Şifre Değiştir Butonu */}
+      <TouchableOpacity style={styles.changeButton}>
+        <Text style={styles.changeButtonText}>Change Password</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9F9FB',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop:30
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+    paddingVertical: 10,
+    fontSize: 16,
+    marginTop: 20,
+  },
+  changeButton: {
+    backgroundColor: '#FF6347',
+    borderRadius: 5,
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  changeButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
